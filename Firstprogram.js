6babb4061c82a7e34aaaa724a820ce3cf2dafdb4
sk2860736-cloud <sk2860736@gmail.com>
2026-03-11 11:34:24 +0530

    Add files via upload

---
+++ Firstprogram.js
@@ -0,0 +1,6 @@
+// variable ko banate hain in java script me 
+
+let name = "sachin";
+age = 20;
+age = 30
+console.log(name,age);
---
+++ day05.js
@@ -0,0 +1,56 @@
+// NUMBERS AND MATH OBJECT LEARN
+
+// let a = 8;
+// let b = 9;
+// console.log(a==b); // ans is false because of the object is non premitive 
+ 
+// let a = 8;
+// let b = a;
+// console.log(a==b); // ans is true 
+
+// numbers 
+
+// let a = new Number(20);
+// let b = new Number(20);
+// console.log(a==b); // because of the number is object and its a non premitive 
+
+// let obj1 = {
+//     name : "sachin"
+// }
+// let obj2=obj1;
+
+
+// console.log(obj1==obj2) // ans is true 
+
+// NON OREMITIVE : REFERENCE BASES PE COMPARE HOTE HAIN 
+// PREMITIVE : COPY BY RULE
+
+// let a = 10;
+// let b = a;
+// console.log(a==b);
+
+
+// MATH USING 
+
+// console.log(Math.abs(-6));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.max(9,67,87,8,6));
+// console.log(Math.min(676,90,-6));
+// console.log(Math.floor(0.6));
+// console.log(Math.ceil(0.6));
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+// console.log(Math.floor(Math.random()*6)+1);
+
+// console.log(Math.floor(Math.random()*11)+15);
+
+console.log(Math.floor(Math.random()*9999-1000)+1000);
+
+// RANDOM NUMBER FORMULA 
+
+// math.floor(math.random()*(max-min)+min);
+
+// console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*11)+15);
